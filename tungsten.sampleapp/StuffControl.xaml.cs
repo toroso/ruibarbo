9971d642c66ca8dec4396ba1747bef1001e60c61
Torbjörn Kalin <tgithub@kalin-urena.com>
2014-05-30 15:32:04 +0200

    More stuff added to test UI.

---
+++ tungsten.sampleapp/StuffControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace tungsten.sampleapp
+{
+    /// <summary>
+    /// Interaction logic for StuffControl.xaml
+    /// </summary>
+    public partial class StuffControl : UserControl
+    {
+        public StuffControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
