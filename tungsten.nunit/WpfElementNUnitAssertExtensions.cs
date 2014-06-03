c2c57f0e15497a070e1e4cdf81fd7af9aa70395e
Torbjörn Kalin <tgithub@kalin-urena.com>
2014-06-03 20:55:53 +0200

    Moved NUnit assertion extensions to own assembly so that users can use their favorite xunit framework.

---
+++ tungsten.nunit/WpfElementNUnitAssertExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using tungsten.core.Elements;
+using tungsten.core.Utils;
+
+namespace tungsten.nunit
+{
+    public static class WpfElementNUnitAssertExtensions
+    {
+        public static void AssertThat<TWpfElement>(
+                this TWpfElement me,
+                Expression<Func<TWpfElement, object>> actualExp,
+                IResolveConstraint resolveConstraint)
+            where TWpfElement : UntypedWpfElement
+        {
+            // TODO: Take default retryTime from config. Share with Core or its own?
+            me.AssertThat(actualExp, resolveConstraint, TimeSpan.FromSeconds(5));
+        }
+
+        public static void AssertThat<TWpfElement>(
+                this TWpfElement me,
+                Expression<Func<TWpfElement, object>> actualExp,
+                IResolveConstraint resolveConstraint,
+                TimeSpan maxRetryTime)
+            where TWpfElement : UntypedWpfElement
+        {
+            Constraint constraint = resolveConstraint.Resolve();
+            var extractFunc = actualExp.Compile();
+
+            var found = WaitUntil(() => constraint.Matches(extractFunc(me)), maxRetryTime);
+            if (!found)
+            {
+                MessageWriter writer = new TextMessageWriter(null, null);
+                writer.WriteMessageLine(string.Empty);
+                constraint.WriteMessageTo(writer);
+                writer.WriteMessageLine("Expr:     {0}", actualExp);
+                writer.WriteMessageLine("Control:  {0} {1}", me.GetType().Name, me.ElementSearchPath());
+                throw new AssertionException(writer.ToString());
+            }
+        }
+
+        private static bool WaitUntil(Func<bool> found, TimeSpan maxRetryTime)
+        {
+            var sleepTime = TimeSpan.FromMilliseconds(10);
+            DateTime retryUntil = DateTime.Now + maxRetryTime;
+            while (DateTime.Now < retryUntil)
+            {
+                if (found())
+                {
+                    return true;
+                }
+
+                Thread.Sleep(sleepTime);
+            }
+
+            return false;
+        }
+    }
+}
--- tungsten.sampletest/CheckBoxTest.cs
+++ tungsten.sampletest/CheckBoxTest.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Linq.Expressions;
-using System.Threading;
-using NUnit.Framework;
-using NUnit.Framework.Constraints;
+﻿using NUnit.Framework;
 using tungsten.core.Elements;
 using tungsten.core.Search;
-using tungsten.core.Utils;
+using tungsten.nunit;
 
 namespace tungsten.sampletest
 {
@@ -17,59 +13,6 @@ namespace tungsten.sampletest
             var window = Desktop.FindFirstElement<WpfWindow>(By.Name("WndMain"));
             var checkBox = window.FindFirstElement<WpfCheckBox>(By.Name("ShowStuff"));
             checkBox.AssertThat(x => x.IsChecked, Is.True);
-        }
-    }
-
-    // TODO: Move to other assembly: tungsten.nunit
-    public static class WpfElementNUnitAssertExtensions
-    {
-        public static void AssertThat<TWpfElement>(
-                this TWpfElement me,
-                Expression<Func<TWpfElement, object>> actualExp,
-                IResolveConstraint resolveConstraint)
-            where TWpfElement : UntypedWpfElement
-        {
-            // TODO: Take default retryTime from config
-            me.AssertThat(actualExp, resolveConstraint, TimeSpan.FromSeconds(5));
-        }
-
-        public static void AssertThat<TWpfElement>(
-                this TWpfElement me,
-                Expression<Func<TWpfElement, object>> actualExp,
-                IResolveConstraint resolveConstraint,
-                TimeSpan maxRetryTime)
-            where TWpfElement : UntypedWpfElement
-        {
-            Constraint constraint = resolveConstraint.Resolve();
-            var extractFunc = actualExp.Compile();
-
-            var found = WaitUntil(() => constraint.Matches(extractFunc(me)), maxRetryTime);
-            if (!found)
-            {
-                MessageWriter writer = new TextMessageWriter(null, null);
-                writer.WriteMessageLine(string.Empty);
-                constraint.WriteMessageTo(writer);
-                writer.WriteMessageLine("Expr:     {0}", actualExp);
-                writer.WriteMessageLine("Control:  {0} {1}", me.GetType().Name, me.ElementSearchPath());
-                throw new AssertionException(writer.ToString());
-            }
-        }
-
-        private static bool WaitUntil(Func<bool> found, TimeSpan maxRetryTime)
-        {
-            var sleepTime = TimeSpan.FromMilliseconds(10);
-            DateTime retryUntil = DateTime.Now + maxRetryTime;
-            while (DateTime.Now < retryUntil)
-            {
-                if (found())
-                {
-                    return true;
-                }
-
-                Thread.Sleep(sleepTime);
-            }
-
-            return false;
         }
     }
 }
