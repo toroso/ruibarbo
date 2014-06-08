6def7fe471ab59a2dd02d64fb1b2c7642cf72757
Torbjörn Kalin <tgithub@kalin-urena.com>
2014-06-08 08:25:42 +0200

    Split CheckBox test into two, give better name.

--- tungsten.sampletest/CheckBoxTest.cs
+++ tungsten.sampletest/CheckBoxTest.cs
@@ -9,11 +9,21 @@ namespace tungsten.sampletest
     public class CheckBoxTest : TestBase
     {
         [Test]
-        public void Hupp()
+        public void CheckBoxIsChecked()
         {
             var window = Desktop.FindFirstChild<WpfWindow>(By.Name("WndMain"));
             var checkBox = window.FindFirstChild<WpfCheckBox>(By.Name("ShowStuff"));
             checkBox.AssertThat(x => x.IsChecked(), Is.True);
+
+            checkBox.Click();
+            checkBox.AssertThat(x => x.IsChecked(), Is.False);
+        }
+
+        [Test]
+        public void CheckBoxChangeIsChecked()
+        {
+            var window = Desktop.FindFirstChild<WpfWindow>(By.Name("WndMain"));
+            var checkBox = window.FindFirstChild<WpfCheckBox>(By.Name("ShowStuff"));
 
             checkBox.Click();
             checkBox.AssertThat(x => x.IsChecked(), Is.False);
