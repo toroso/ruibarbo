ff0d9f33945d3ca9a16213d2a2d94873e536ab4d
Torbjörn Kalin <tgithub@kalin-urena.com>
2014-07-11 23:24:47 +0200

    Moved HwndWrapper to remove circular dependency.

--- tungsten.core/Win32/HwndWrapper.cs
+++ tungsten.core/Win32/HwndWrapper.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace tungsten.core.Win32.Factory
+namespace tungsten.core.Win32
 {
     // Not to be confused with the Microsoft.VisualStudio.PlatformUI.HwndWrapper class
     internal class HwndWrapper
