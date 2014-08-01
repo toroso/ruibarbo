f815a4cbeaeed29e063cedf68afe87ac608e25dd
Torbjörn Kalin <torbjorn.kalin@wasakredit.se>
2014-08-01 09:33:53 +0200

    Support for ToggleButton.

---
+++ ruibarbo.core/Wpf/Base/WpfToggleButtonBase.cs
@@ -0,0 +1,23 @@
+﻿using ruibarbo.core.ElementFactory;
+using ruibarbo.core.Wpf.Invoker;
+
+namespace ruibarbo.core.Wpf.Base
+{
+    public class WpfToggleButtonBase<TNativeElement> : WpfFrameworkElementBase<TNativeElement>
+        where TNativeElement : System.Windows.Controls.Primitives.ToggleButton
+    {
+        public WpfToggleButtonBase(ISearchSourceElement searchParent, TNativeElement checkBox)
+            : base(searchParent, checkBox)
+        {
+        }
+    }
+
+    public static class WpfToggleButtonBaseExtensions
+    {
+        public static bool? IsChecked<TNativeElement>(this WpfToggleButtonBase<TNativeElement> me)
+            where TNativeElement : System.Windows.Controls.Primitives.ToggleButton
+        {
+            return OnUiThread.Get(me, frameworkElement => frameworkElement.IsChecked);
+        }
+    }
+}
---
+++ ruibarbo.core/Wpf/WpfToggleButton.cs
@@ -0,0 +1,15 @@
+﻿using ruibarbo.core.ElementFactory;
+using ruibarbo.core.Wpf.Base;
+using ruibarbo.core.Wpf.Factory;
+
+namespace ruibarbo.core.Wpf
+{
+    [RegisteredElement]
+    public sealed class WpfToggleButton : WpfToggleButtonBase<System.Windows.Controls.Primitives.ToggleButton>
+    {
+        public WpfToggleButton(ISearchSourceElement searchParent, System.Windows.Controls.Primitives.ToggleButton frameworkElement)
+            : base(searchParent, frameworkElement)
+        {
+        }
+    }
+}
