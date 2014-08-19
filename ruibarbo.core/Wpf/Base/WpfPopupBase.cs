2ada6aa67bdf5fc9a171e4ccd48c1eb0be5b8526
Torbjörn Kalin <tgithub@kalin-urena.com>
2014-08-19 09:52:13 +0200

    Support for Popup control.

---
+++ ruibarbo.core/Wpf/Base/WpfPopupBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ruibarbo.core.ElementFactory;
+using ruibarbo.core.Wpf.Invoker;
+
+namespace ruibarbo.core.Wpf.Base
+{
+    public class WpfPopupBase<TNativeElement> : WpfFrameworkElementBase<TNativeElement>
+        where TNativeElement : System.Windows.Controls.Primitives.Popup
+    {
+        public WpfPopupBase(ISearchSourceElement searchParent, TNativeElement frameworkElement)
+            : base(searchParent, frameworkElement)
+        {
+        }
+
+        public override IEnumerable<object> NativeChildren
+        {
+            get
+            {
+                var root = OnUiThread.Get(this, frameworkElement => frameworkElement.Child);
+
+                if (root != null)
+                {
+                    yield return root;
+                }
+            }
+        }
+    }
+}
---
+++ ruibarbo.core/Wpf/WpfPopup.cs
@@ -0,0 +1,15 @@
+﻿using ruibarbo.core.ElementFactory;
+using ruibarbo.core.Wpf.Base;
+using ruibarbo.core.Wpf.Factory;
+
+namespace ruibarbo.core.Wpf
+{
+    [RegisteredElement]
+    public sealed class WpfPopup : WpfPopupBase<System.Windows.Controls.Primitives.Popup>
+    {
+        public WpfPopup(ISearchSourceElement searchParent, System.Windows.Controls.Primitives.Popup frameworkElement)
+            : base(searchParent, frameworkElement)
+        {
+        }
+    }
+}
