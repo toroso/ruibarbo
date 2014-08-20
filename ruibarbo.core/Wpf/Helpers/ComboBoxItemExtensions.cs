6c9b8995af762e7049bc4118c790650f51cfe04f
Torbjörn Kalin <tgithub@kalin-urena.com>
2014-08-20 08:53:49 +0200

    Moved the delay for opening ComboBoxes to be after the IsVisible check.

--- ruibarbo.core/Wpf/Helpers/ComboBoxItemExtensions.cs
+++ ruibarbo.core/Wpf/Helpers/ComboBoxItemExtensions.cs
@@ -18,16 +18,16 @@ namespace ruibarbo.core.Wpf.Helpers
             var itemsContainer = me.FindFirstAncestor<WpfComboBox>();
             itemsContainer.Open();
 
-            // Don't know why I sometimes need this. IsClickable reports true, but still an item in the ComboBox is not clickable.
-            if (Configuration.DelayWhenOpeningComboBox > TimeSpan.Zero)
-            {
-                System.Threading.Thread.Sleep(Configuration.DelayWhenOpeningComboBox);
-            }
-
             bool isVisible = Wait.Until(() => me.IsVisible);
             if (!isVisible)
             {
                 throw RuibarboException.StateFailed(me, x => x.IsVisible);
+            }
+
+            // Don't know why I sometimes need this. IsClickable reports true, but still an item in the ComboBox is not clickable.
+            if (Configuration.DelayWhenOpeningComboBox > TimeSpan.Zero)
+            {
+                System.Threading.Thread.Sleep(Configuration.DelayWhenOpeningComboBox);
             }
 
             me.Click();
